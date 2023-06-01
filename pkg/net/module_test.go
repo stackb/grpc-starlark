db8165c8fbadc9500fd9336810bfc43cf9a9bc6b
Paul Johnston <pcj@stack.build>
2023-06-01 09:45:12 -0600

    more test cases

--- pkg/net/module_test.go
+++ pkg/net/module_test.go
@@ -17,6 +17,26 @@ func TestNetModule(t *testing.T) {
 			input: "net.Listener",
 			want:  "<built-in function net.Listener>",
 		},
+		{
+			input: "net.Listener(network = 'tcp', address='localhost:1301')",
+			want:  "<net.Listener tcp 127.0.0.1:1301>",
+		},
+		{
+			input: "net.Listener().network",
+			want:  `"tcp"`,
+		},
+		{
+			input: "net.Listener(address='localhost:1300').address",
+			want:  `"127.0.0.1:1300"`,
+		},
+		{
+			input: "net.Listener(address=':1302')",
+			want:  "<net.Listener tcp [::]:1302>",
+		},
+		{
+			input: "net.Listener(address=':1303')",
+			want:  "<net.Listener tcp [::]:1303>",
+		},
 	}
 
 	for _, tc := range testCases {
