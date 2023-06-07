4399203ab6938b8652c82339538b3ebe014a4eef
Paul Johnston <pcj@stack.build>
2023-06-07 12:32:36 -0600

    remove dead code

--- cmd/grpcstar/grpcstar.go
+++ cmd/grpcstar/grpcstar.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -14,16 +13,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// c := make(chan os.Signal, 1)
-	// signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-
 	if err := run(cwd, os.Args[1:]); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-
-	// <-c
 }
 
 func run(wd string, args []string) error {
@@ -42,28 +35,4 @@ func run(wd string, args []string) error {
 	}
 
 	return nil
-}
-
-func help() error {
-	fmt.Fprint(os.Stderr, `usage: grpcstar <command> [args...]
-
-grpcstar is a standalone interpreter for a dialect of starlark intended
-for interaction with gRPC services.
-
-grpcstar may be run with one of the commands below. If no command is given,
-grpcstar defaults to "eval".
-
-  run - grpcstar will load the given args as filenames.
-  help - show this message.
-
-For usage information for a specific command, run the command with the -h flag.
-For example:
-
-  grpcstar run -h
-
-grpcstar is under active development, and its interface may change
-without notice.
-
-`)
-	return flag.ErrHelp
 }
